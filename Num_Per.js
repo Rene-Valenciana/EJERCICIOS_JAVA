9828529ea0cb40a22a7a2bd6ad1550332a36ccd9
ReneValenciana <alexvalenciana02@gmail.com>
2022-10-12 11:02:28 -0500

    se comenzo el ejercicio de numeros perfectos

---
+++ Num_Per.js
@@ -0,0 +1,25 @@
+Muestras = Number(prompt("¿Cuantos numeros desea ver"));
+
+
+for (i = 1; i<=Muestras;) {
+    n=1
+    for(n=1;n<=i;){
+      div=  
+    }
+     
+    if ( toma>= 24) {
+        hora= -1*(24-toma) 
+        toma= hora
+        if (toma = 0){
+            toma= 0
+        }
+        console.log("toma ", i, ": ", toma, " horas");
+        i= i+1
+        toma=toma+hora_frec
+    }
+    else {
+        console.log("toma ", i, ": ", toma, " horas");
+        i= i+1
+        toma=toma+hora_frec
+    }
+}
