8f0f8b622219b97413d9fb6f9ae9e586245f38ed
ReneValenciana <alexvalenciana02@gmail.com>
2022-10-12 10:38:26 -0500

    se cambio los nombres de los archivos

--- medicamentos.js
+++ medicamentos.js
@@ -1,17 +1,22 @@
 hora_inic = Number(prompt("¿A que hora tomo su medicamento? (24 horas)"));
 hora_frec = Number(prompt("¿Con que frecuencia toma su medicamento?"));
-toma = hora_inic + hora_frec
-
-for (i = 0; i<=12;) {
-    if ( toma> 24) {
+toma= hora_inic
+for (i=1; i<=12; i++) {
+   
+    if ( toma>= 24) {
         hora= -1*(24-toma) 
         toma= hora
         console.log("toma ", i, ": ", toma, " horas");
-        i= i+1
     }
     else {
-        toma=toma
+        if (i=1) {
+            toma=toma
+        }
+        else{
+            toma=toma+hora_frec 
+        
+        }
         console.log("toma ", i, ": ", toma, " horas");
-        i= i+1
     }
+
 }
