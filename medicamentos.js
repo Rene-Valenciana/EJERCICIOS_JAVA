37af20ee6ff34941d8e58560b2c6d2429c7dee61
ReneValenciana <alexvalenciana02@gmail.com>
2022-10-12 10:52:56 -0500

    se completo el ejercicio correctamente

--- medicamentos.js
+++ medicamentos.js
@@ -1,22 +1,22 @@
 hora_inic = Number(prompt("¿A que hora tomo su medicamento? (24 horas)"));
 hora_frec = Number(prompt("¿Con que frecuencia toma su medicamento?"));
-toma= hora_inic
-for (i=1; i<=12; i++) {
-   
+toma = hora_inic 
+
+for (i = 1; i<=12;) {
     if ( toma>= 24) {
         hora= -1*(24-toma) 
         toma= hora
+        if (toma = 0){
+            toma= 0
+        }
         console.log("toma ", i, ": ", toma, " horas");
+        i= i+1
+        toma=toma+hora_frec
     }
     else {
-        if (i=1) {
-            toma=toma
-        }
-        else{
-            toma=toma+hora_frec 
-        
-        }
         console.log("toma ", i, ": ", toma, " horas");
+        i= i+1
+        toma=toma+hora_frec
     }
-
 }
+
